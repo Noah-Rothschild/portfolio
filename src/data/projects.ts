5773de82ca7144ea7502b26b9ebf813cf8e77fc5
iNoah-Guy <76164284+Noah-Rothschild@users.noreply.github.com>
2026-07-25 13:31:10 +0200

    Add Project type and the initial project data

---
+++ src/data/projects.ts
@@ -0,0 +1,36 @@
+import { Project } from "../types/project";
+
+const projects: Project[] = [
+    {
+        title: "chefbook",
+        description: "Webapp Project where a user adds ingredients to their personal pantry and is able to have recipes suggested to them based on the contents of their pantry.",
+        status: "in-progress",
+        image: "",
+        liveUrl: "",
+        githubUrl: "https://github.com/Noah-Rothschild/chefbook",
+        
+    },
+
+    {
+        title: "rising-tide",
+        description: "C-2-C E-commerce website built using Javascript, PHP and MySQL that aims to connect South African vendors to customers in a secure marketplace environment.",
+        status: "live",
+        image: "",
+        liveUrl: "rising-tide.freedev.app/",
+        githubUrl: "https://github.com/Noah-Rothschild/rising-tide",
+        
+    },
+
+    {
+        title: "tourism-safety app",
+        description: "Location-based system that provides crowd-sourced safety information about Cape Town aimed at increasing tourism safety within the city.",
+        status: "in-progress",
+        image: "",
+        liveUrl: "",
+        githubUrl: "",
+        
+    }
+
+];
+
+export default projects;
