4ed88ad6e0dce2f95a0d79594bf50ed7c31df30a
iNoah-Guy <76164284+Noah-Rothschild@users.noreply.github.com>
2026-08-12 15:52:34 +0200

    Added about page content and CV download link

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
 export default function AboutPage() {
-  return <div>About page placeholder</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Hello, I'm Noah Rothschild! I'm a software engineer with a passion for building full-stack solutions. My current experience includes leveraging modern web technologies such as Django, Typescript, React and Next.js to create efficient and scalable web-solutions. I am always looking for new challenges and learning opportunities and am currently developing my cloud architecture knowledge.</p>
+      <a href="/noah_rothschild_cv.pdf" download>Download Resume</a>
+    </div>
+  );
 }
