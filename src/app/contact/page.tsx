1c4fc88a0e47c8c451e8f0b9434207e7da1781ad
iNoah-Guy <76164284+Noah-Rothschild@users.noreply.github.com>
2026-08-14 15:50:33 +0200

    Added contact page content and associated links to Social Media and email

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,22 @@
+
+
 export default function ContactPage() {
-  return <div>Contact page placeholder</div>;
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>If you have a project that needs help or a role that you think I'd be a good fit for, feel free to reach out!</p>
+
+      <h2>Location</h2>
+      <p>Cape Town, South Africa.</p>
+
+      <h2>Email</h2>
+      <p><a href="mailto:nprothschild@gmail.com">nprothschild@gmail.com</a></p>
+
+      <h2>Social Media</h2>
+      <p><a href="https://www.linkedin.com/in/noah-rothschild/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+      <p><a href="https://github.com/Noah-Rothschild" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+
+    </div>
+  );
 }
+
